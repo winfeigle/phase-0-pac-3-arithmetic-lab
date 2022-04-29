30ab5ccfedb4dac4abdc0bd6873801d41e20f765
Win Feigle <win.feigle@gmail.com>
2022-04-29 12:09:20 -0600

    commit tree 530b69ea163031bd86b400c8881613d9526ce68f

---
+++ index.js
@@ -0,0 +1,15 @@
+const add = (a, b) => { return a + b; }
+
+const subtract = (a, b) => { return a - b; }
+
+const multiply = (a, b) => { return a * b; }
+
+const divide = (a, b) => { return a/b; }
+
+const increment = n => { return ++n; }
+
+const decrement = n => { return --n; }
+
+const makeInt = n => {return parseInt(n, 10); }
+
+const preserveDecimal = n => {return parseFloat(n); }
